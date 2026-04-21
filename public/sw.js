218920f070fa2df4d30b11a42a221e4e3d4669f3
mrnafi1 <nafimr718@gmail.com>
2026-04-21 11:06:27 +0600

    Add professional PWA manifest and service worker for app installation

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,19 @@
-const CACHE_NAME = 'nafinance-v1';
-const assets = ['/'];
+const CACHE_NAME = 'nafinance-premium-v1';
+const assetsToCache = ['/', '/index.html'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('NaFinance: Performance Cache Established');
+      return cache.addAll(assetsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
